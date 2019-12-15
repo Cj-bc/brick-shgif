99560439b7fbeaf615d7dce201807fb3a58b7c31
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-16 03:51:16 +0900

    Add: Shgif/Type> shgifToCanvas -- convert Shgif to Tart.Canvas

--- src/Shgif/Type.hs
+++ src/Shgif/Type.hs
@@ -2,15 +2,20 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Shgif.Type where
+module Shgif.Type (
+    Format(..), Shgif(..)
+    , shgifToCanvas
+) where
 
 import GHC.Generics (Generic)
 import Control.Lens (makeLenses)
 import Data.HashMap.Lazy ((!))
 import qualified Data.Vector as V
 import Data.Text (unpack)
+import Data.Maybe (fromMaybe)
 import Data.Yaml (FromJSON(..), withObject, (.:), Object(..), withArray
                  , Parser(..), Value(..))
+import Tart.Canvas (Canvas, canvasFromText)
 
 -- | format  of shgif file
 data Format = Page -- ^ list data as list of String
@@ -60,3 +65,14 @@ parseContents :: Value -> Parser [String]
 parseContents = withArray "contents" $ \a -> return $ V.toList $ V.map (toStr) a
     where
         toStr (String a) = unpack a
+
+
+-- | Convert Shgif into Tart.Canvas datatype
+-- This function only determine which frame to render, and pass it to @canvasFromText@
+shgifToCanvas :: Shgif -> IO Canvas
+shgifToCanvas (Shgif _ _ _ w h tick ds) =
+    let currentFrame t = fromMaybe (currentFrame (t-1)) $ lookup t ds
+        frame = currentFrame tick :: [String]
+    in canvasFromText $ unlines frame
+
+

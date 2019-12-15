2643eb97cc7430ca2944f1ecb1b0717c00990b28
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-15 16:59:13 +0900

    Add: writing yaml parser [WIP]

--- src/Shgif/Type.hs
+++ src/Shgif/Type.hs
@@ -1,27 +1,57 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Shgif.Type where
 
-import GHC.Generics
-import Control.Lens
-
+import GHC.Generics (Generic)
+import Control.Lens (makeLenses)
+import Data.HashMap.Lazy ((!))
+import qualified Data.Vector as V
+import Data.Yaml (FromJSON(..), withObject, (.:), Object(..), withArray
+                 , Parser(..), Value(..))
 
 -- | format  of shgif file
 data Format = Page -- ^ list data as list of String
             | Plot -- ^ list data based on its coordinate
+            deriving (Generic, Show)
 
-data Shgif = Shgif { _title :: String
-                   , _author :: String
-                   , _format :: Format
-                   , _width :: Int
-                   , _heigh :: Int
-                   , _data :: [(Float, [String])]  -- ^ [(Time, String to write)]
-                    } deriving (Generic)
+type TimeStamp = (Float, [String])
 
-mkLenses ''Shgif
+timeStamp :: Float -> [String] -> TimeStamp
+timeStamp time ds = (time, ds)
 
-instance FromJSON Shgif
+data Shgif = Shgif { _title     :: String
+                   , _author    :: String
+                   , _format    :: Format
+                   , _width     :: Int
+                   , _heigh     :: Int
+                   , _shgifData :: [TimeStamp]  -- ^ [(Time, String to write)]
+                    } deriving (Show)
+
+makeLenses ''Shgif
+
+instance FromJSON Format
 
 
+instance FromJSON Shgif where
+  parseJSON = withObject "Shgif" $ \v -> Shgif
+        <$> v .: "title"
+        <*> v .: "author"
+        <*> v .: "format"
+        <*> v .: "width"
+        <*> v .: "heigh"
+        <*> parseFrame (v ! "data")
 
+
+parseFrame :: Value -> Parser [TimeStamp]
+parseFrame = withArray "data" $ \a -> sequence $ V.toList $ V.map parseTimeStamp a
+
+
+parseTimeStamp :: Value -> Parser TimeStamp
+parseTimeStamp = withObject "Frame" $ \f -> timeStamp
+                    <$> f .: "timestamp"
+                    <*> parseContents (f ! "contents")
+
+parseContents :: Value -> Parser [String]
+parseContents = withArray "contents" $ \a -> return $ V.toList $ V.map show a

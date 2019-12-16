1da67d4e7e5d9eb14c771eaa4579da1c7b27c1a9
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-16 21:09:18 +0900

    Fix: app/Main> apply 'Name' so that it won't be ambiguous

--- app/Main.hs
+++ app/Main.hs
@@ -12,9 +12,10 @@ import Brick.Extensions.Shgif.Widgets (shgif)
 import Shgif.Type (Shgif(..), updateShgif, addInitialCanvas)
 
 type AppState = Shgif
+data Name = NoName deriving (Eq, Ord)
 
 -- You should use 'TickEvent' as Event type
-app :: App AppState TickEvent n
+app :: App AppState TickEvent Name
 app = App {appDraw = ui
           , appHandleEvent = eHandler
           , appStartEvent = return
@@ -23,7 +24,7 @@ app = App {appDraw = ui
 ui :: AppState -> [Widget Name]
 ui s = [shgif s]
 
-eHandler :: AppState -> BrickEvent n TickEvent -> EventM n (Next AppState)
+eHandler :: AppState -> BrickEvent Name TickEvent -> EventM Name (Next AppState)
 eHandler s (VtyEvent (Vty.EvKey (Vty.KChar 'q') [])) = halt s
 eHandler s (AppEvent Tick)  = liftIO (updateShgif s) >>= continue
 

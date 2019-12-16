9deb80045be09d9dc2589cfb7bf7e9712c7f6af8
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-16 17:09:11 +0900

    Add: app/Main> adding example main source that use Shgif

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
-import Lib
+import Data.Yaml (decodeFileEither, ParseException)
+import System.Exit (exitFailure)
+import Brick
+import Brick.Extensions.Shgif.Events
+
+import Shgif.Type
 
 main :: IO ()
-main = someFunc
+main = do
+    -- Read Shgif data from File
+    sgf <- case (decodeFileEither "filename" :: Either ParseException Shgif) of
+                -- Complete initializing Shgif by adding @Canvas@
+                Right y -> return $ addInitialCanvas y
+                Left e  -> putStrLn $ show e >> exitFailure
+
+    -- Include sgf into your 'State'
+
+    finalState <- mainWithTick Nothing 1000 (App here) (State here)

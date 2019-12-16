8f723dfcbd66a15d490757137a05dd23c134b178
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-16 16:39:43 +0900

    Fix: Brick/Extensions/Shgif/Events> TickEvent has no value; fix type for mainWithTick

--- src/Brick/Extensions/Shgif/Events.hs
+++ src/Brick/Extensions/Shgif/Events.hs
@@ -7,21 +7,26 @@ import Brick
 import Brick.BChan
 import Graphics.Vty
 
-data TickEvent = Tick Int
+data TickEvent = Tick
 
 -- Create @customMain@ with Tick event support
 --  difference between this & customMain:
---    - Generate 'TickEvent' for specified 
+--    - Create new thread to Generate TickEvent
+--    - Generate 'TickEvent' every 'tickRate' microseconds.
+--
+-- you should count up something in event handler
+-- if you want to know how much time does TickEvent ran.
 --
 -- Those codes are from tutorials:
 -- - jtdaugherty/brick/docs/samtay-tutorial.md @46feab9bfef540e62bbe05424297959ece2a711a
 -- - jtdaugherty/brick/docs/guide.rst#using-your-own-event-type @46feab9bfef540e62bbe05424297959ece2a711a
-mainWithTick :: Maybe Int -> Int -> App s (Int -> TickEvent) n -> state -> IO state
+mainWithTick :: Ord n => Maybe Int -> Int -> App state TickEvent n -> state -> IO state
 mainWithTick chanCapacity tickRate app initialState = do
     eventChan <- Brick.BChan.newBChan $ fromMaybe 10 chanCapacity
+
     forkIO $ forever $ do
       writeBChan eventChan Tick
       threadDelay tickRate
+
     let buildVty = Graphics.Vty.mkVty Graphics.Vty.defaultConfig
     customMain buildVty (Just eventChan) app initialState
-    -- Use finalState and exit

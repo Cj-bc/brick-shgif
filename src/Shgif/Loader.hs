5809084e836fa9a929e973da5f477e2fe7f851ed
Cj-bc <cj.bc-sd@outlook.jp>
2020-02-23 09:47:58 +0900

    Change: Shgif.Type> Split module into few modules

---
+++ src/Shgif/Loader.hs
@@ -0,0 +1,84 @@
+module Shgif.Loader (
+    -- * from original format
+      getShgif
+    , getShgifs
+
+    -- * 'Tart' related
+    , fromCanvas
+    , fromTartFile
+) where
+import Control.Lens ((&), (+~), (^.))
+import Shgif.Type (Shgif(..), Format(..))
+import Shgif.Type.Internal (TimeStamp, addInitialCanvas)
+import Data.Yaml (ParseException, decodeFileEither)
+import Data.Either (isLeft)
+import Tart.Format (TartFile(..), sortedCanvases)
+import Tart.Canvas (Canvas, canvasSize, prettyPrintCanvas
+                   )
+
+defaultTimeStampInterval = 100
+
+
+
+-- | Get 'Shgif' data from Yaml file
+getShgif :: FilePath -> IO (Either ParseException Shgif)
+getShgif n = do
+    sgf <- (decodeFileEither n :: IO (Either ParseException Shgif))
+    case sgf of
+      Left e -> return $ Left e
+      Right shgif -> do
+        sgf' <- addInitialCanvas shgif
+        return $ Right sgf'
+
+
+-- | Get list of 'Shgif's from list of Yaml file
+getShgifs :: [FilePath] -> IO (Either [ParseException] [Shgif])
+getShgifs xs = do
+  results <- sequence $ map getShgif xs :: IO [Either ParseException Shgif]
+  if (containsLeft results)
+    then return $ Left  $ caughtExceptions results
+    else return $ Right $ map fromRight results
+  where
+    containsLeft rs     = True `elem` map isLeft rs
+    fromLeft (Left e)   = e
+    fromRight (Right a) = a
+    caughtExceptions rs = map fromLeft $ filter isLeft rs
+
+
+-- | Create 'Shgif' from 'Tart.Canvas.Canvas'es without any meta value
+--
+-- If you can implement meta values, use 'fromCanvasWithMeta' instead.
+--
+-- The first argument specify the tick for each frame.
+--
+-- If 'Nothing', use 'defaultTimeStampInterval'
+fromCanvas :: Maybe [Int] -> [Canvas] -> Shgif
+fromCanvas (Just ts)  cs = fromCanvasWithMeta "" "" ts cs
+fromCanvas Nothing cs = fromCanvasWithMeta "" "" defaultTimestamps cs
+    where
+        defaultTimestamps = take (length cs) $ 0: interval 0 defaultTimeStampInterval
+        interval orig i = orig + i: interval (orig + i) i
+
+-- | Create 'Shgif' from 'Tart.Format.TartFile'
+fromTartFile :: Maybe [Int] -> TartFile -> Shgif
+fromTartFile ix tartFile = let cs  = sortedCanvases (tartFileCanvasOrder tartFile) (tartFileCanvasList tartFile)
+                           in        fromCanvas Nothing $ cs ++ [last cs]
+
+
+-- | Create 'Shgif' from 'Tart.Canvas.Canvas' with meta value
+--
+-- This only support 'Page' format, because 'Tart.Canvas.Canvas' is Bitmap image.
+--
+-- For __Not__ 'updateShgifTo' use cases:
+-- Make sure to __duplicate the last Canvas__ so that it'll show up for more than 1 tick.
+--
+-- (If you don't, The last frame only appear for 1 tick. In most case, it's the same as invisible)
+fromCanvasWithMeta :: String -> String -> [Int] -> [Canvas] -> Shgif
+fromCanvasWithMeta title author timestamps cs = Shgif title author Page w h 0 convertedData Nothing
+  where
+    (w, h) = foldr1 (\(x,y ) (x', y') -> (max x x', max y y')) whList
+    whList = fmap (canvasSize) cs
+    convertedData :: [TimeStamp]
+    convertedData = zip timestamps $ fmap (lines . prettyPrintCanvas False . pure) cs
+
+
---
+++ src/Shgif/Updater.hs
@@ -0,0 +1,96 @@
+{-|
+Module      : Shgif.Updater
+Description : Collection of function which update Shgif status
+Copyright   : (c) Cj.bc_sd a.k.a Cj-bc, 2020
+Maintainer  : cj.bc-sd@outlook.jp
+Stability   : experimental
+
+This module provides lots kind of updater.
+-}
+module Shgif.Updater (
+      updateShgifNoLoop
+    , updateShgif
+    , updateShgifReversedNoLoop
+    , updateShgifReversed
+    , updateShgifTo
+) where
+import Shgif.Type.Internal
+import Control.Lens (over, set, (+~), (&), (^.))
+
+
+-- | Update 'Shgif''s internal tick state, which will affect frame rendering.
+--
+-- As 'updateShgif' has type `Shgif -> IO Shgif`, it can be called inside 'Brick.EventM' monad.
+--
+-- This function __won't loop__ gif.
+--
+-- Use this if you want to show animation only once.
+updateShgifNoLoop :: Shgif -> IO Shgif
+updateShgifNoLoop shgif@(Shgif t a f w h tick ds c) = do
+    let updateTick | tick <= lastTimeStamp = over currentTick (+ 1)
+                   | otherwise             = id
+
+    newC <- shgifToCanvas $ updateTick shgif
+
+    return $ set canvas (Just newC) $ updateTick shgif
+    where
+        lastTimeStamp = maximum $ map fst ds
+
+
+-- | Update 'Shgif''s internal tick state, which will affect frame rendering.  
+--
+-- As 'updateShgif' has type `Shgif -> IO Shgif`, it can be called inside brick's 'Brick.EventM' monad.
+--
+-- This function reverse and __won't loop__ gif.
+--
+-- Use this if you want to show reversed animation for only once.
+updateShgifReversedNoLoop :: Shgif -> IO Shgif
+updateShgifReversedNoLoop shgif = do
+    let updateTick | 0 < (shgif^.currentTick) = over currentTick (subtract 1)
+                   | otherwise                = id
+    newC <- shgifToCanvas $ updateTick shgif
+    return $ set canvas (Just newC) $ updateTick shgif
+
+
+-- | Update 'Shgif''s internal tick state, which will affect frame rendering.  
+--
+-- As 'updateShgif' has type `Shgif -> IO Shgif`, it can be called inside brick's 'Brick.EventM' monad.
+--
+-- This function reverse gif.
+--
+-- Use this if you want to show reversed animation.
+updateShgifReversed :: Shgif -> IO Shgif
+updateShgifReversed shgif = do
+    let updateTick = set currentTick (repeat lastTimeStamp $ (shgif^.currentTick) - 1)
+    newC <- shgifToCanvas $ updateTick shgif
+    return $ set canvas (Just newC) $ updateTick shgif
+    where
+        lastTimeStamp = maximum $ map fst (shgif^.shgifData)
+        -- https://docs.unity3d.com/ja/2019.2/ScriptReference/Mathf.Repeat.html
+        repeat max val | val < 0   = max
+                       
+-- | Update 'Shgif''s internal tick state, which will affect frame rendering.  
+--
+-- As 'updateShgif' has type `Shgif -> IO Shgif`, it can be called inside brick's 'Brick.EventM' monad.
+--
+updateShgif :: Shgif -> IO Shgif
+updateShgif shgif = do
+    let updateTick = set currentTick (repeat lastTimeStamp $ (shgif^.currentTick) + 1)
+    newC <- shgifToCanvas $ updateTick shgif
+    return $ set canvas (Just newC) $ updateTick shgif
+    where
+        lastTimeStamp = maximum $ map fst (shgif^.shgifData)
+        -- https://docs.unity3d.com/ja/2019.2/ScriptReference/Mathf.Repeat.html
+        repeat max val | max < val = 0
+                       | otherwise = val
+
+
+-- | Update 'Shgif''s internal tick state to make it closer to given tick
+updateShgifTo :: Int -> Shgif -> IO Shgif
+updateShgifTo tick shgif  = do
+    let tickToAdd = case (shgif^.currentTick) `compare` tick of
+                        LT -> 1
+                        EQ -> 0
+                        GT -> -1
+    newC <- shgifToCanvas (shgif&currentTick+~tickToAdd)
+    return $ set canvas (Just newC) $ over currentTick (+ tickToAdd) shgif
